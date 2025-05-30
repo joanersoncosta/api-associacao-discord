c7660dfb8a5451295ea65f86319bb3e3b2abfed0
joanersoncosta <109226312+joanersoncosta@users.noreply.github.com>
2025-05-29 21:08:33 -0300

    Update DiscordBotConfig.java

--- src/main/java/com/example/demo/config/DiscordBotConfig.java
+++ src/main/java/com/example/demo/config/DiscordBotConfig.java
@@ -10,24 +10,37 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Log4j2
 @Configuration
+@EnableAsync
 @RequiredArgsConstructor
 public class DiscordBotConfig {
 
     @Value("${discord.bot.token}")
     private String botToken;
+
     private final MemberJoinListener memberJoinListener;
 
+    @Async
     @PostConstruct
-    public void startBot() throws Exception {
+    public void startBot() {
         log.info("[start] DiscordBotConfig - startBot");
-        log.info("[start] DiscordBotConfig - {}", botToken);
+        log.info("[token] {}", botToken);
+
         JDABuilder.createDefault(botToken)
                 .enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(memberJoinListener)
-                .build();
-        log.debug("[finish] DiscordBotConfig - startBot");
+                .buildAsync()
+                .thenAccept(jda -> log.info("JDA iniciado com sucesso"))
+                .exceptionally(error -> {
+                    log.error("Erro ao iniciar JDA", error);
+                    return null;
+                });
     }
+}
 }
